b1a82545c46f4a856661eb204beb778c6c623660
Elijas Stekovics <elijas.st@gmx.at>
2020-11-05 10:30:00 +0100

    Projektdateien hinzufügen.

---
+++ RQE-Exam/SpecFlowFeature1.feature
@@ -0,0 +1,23 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the result of a trigonometric function
+
+@mytag
+Scenario: The Sinus Function
+	Given the first number is PI
+	And the chosen function is sinus
+	When this combinatin is choosen
+	Then the result should be 0
+
+Scenario: The Cosinus Function
+	Given the first number is PI
+	And the chosen function is cosinus
+	When this combinatin is choosen
+	Then the result should be -1
+
+Scenario: The Tangens Function
+	Given the first number is PI
+	And the chosen function is tangens
+	When this combinatin is choosen
+	Then the result should be 0
---
+++ RQE-Exam/SpecFlowFeature1Steps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQE_Exam
+{
+    
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double firstNumber, calculatedNumber;
+        [Given(@"the first number is PI")]
+        public void GivenTheFirstNumberIsPI()
+        {
+            firstNumber = Math.PI;
+        }
+        
+        [Given(@"the chosen function is sinus")]
+        public void GivenTheChosenFunctionIsSinus()
+        {
+            calculatedNumber = Math.Sin(firstNumber);
+        }
+        
+        [Given(@"the chosen function is cosinus")]
+        public void GivenTheChosenFunctionIsCosinus()
+        {
+            calculatedNumber = Math.Cos(firstNumber);
+        }
+        
+        [Given(@"the chosen function is tangens")]
+        public void GivenTheChosenFunctionIsTangens()
+        {
+            calculatedNumber = Math.Tan(firstNumber);
+        }
+        
+        [When(@"this combinatin is choosen")]
+        public void WhenThisCombinatinIsChoosen()
+        {
+ 
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            int expected = p0;
+            double actual = calculatedNumber;
+            Assert.Equal(expected,actual,5);
+        }
+    }
+}
